0bcc8269386e6d52cb75ac4d0f04dd9fdffe582d
Divyang Kankotiya <dh304k@gmail.com>
2017-08-27 14:46:08 -0400

    commit

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ function fngetDataChkOut()
     i = document.location.href.split('?')[1].split('=')[1];
     document.getElementById("divHiddenChkout").innerHTML = i;
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET" , "pages/guitar.json" , true);
+    xhttp.open("GET" , "./pages/guitar.json" , true);
     xhttp.send();
     xhttp.onreadystatechange = function ()
     {
@@ -46,7 +46,7 @@ function fngetDataChkOut()
         {
             var varJasonData = JSON.parse(xhttp.responseText);
             var obj = varJasonData.allProducts[i];
-            document.getElementById("imgchkout").src = "'../" + obj.image_path + "'";
+            document.getElementById("imgchkout").src = obj.image_path;
             document.getElementById("divDescChkOut").innerHTML = "<b>Description : </b>" + obj.product_description;
         }
     };
